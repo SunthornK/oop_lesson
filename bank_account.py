0fd4b546e795af84901de5a7794309a30bca4e50
Sunthorn Kompita <sunthorn.ko@ku.th>
2023-11-02 10:04:15 +0700

    add deleted method

--- bank_account.py
+++ bank_account.py
@@ -1,3 +1,37 @@
+class AccountDB:
+    def __init__(self):
+        self.account_database = []
+
+    def insert(self, account):
+        index = self.__search_private(account.account_number)
+        if index == -1:
+            self.account_database.append(account)
+        else:
+            return print("Duplicated account")
+
+    def delete_account(self, account):
+        index = self.__search_private(account.account_number)
+        if index != -1:
+            del self.account_database[index]
+        else:
+            print("invalid account number; nothing to be deleted.")
+
+    def __search_private(self, account_num):
+        for i in range(len(self.account_database)):
+            if self.account_database[i].account_number == account_num:
+                return i
+        return -1
+
+    def search_public(self, account_num):
+        for account in self.account_database:
+            if account.account_number == account_num:
+                return account
+        return None
+
+    def __str__(self):
+        return ''.join(f"{item}, " for item in self.account_database)
+
+
 class Account:
     def __init__(self, num, acc_type, account, balance):
         self.account_number = num
@@ -15,3 +49,20 @@ class Account:
     def __str__(self):
         return f'{{{self.account_number}, {self.type}, {self.account_name}, {self.balance}}}'
 
+
+account1 = Account("0000", "saving", "David Patterson", 1000)
+account2 = Account("0001", "checking", "John Hennessy", 2000)
+account3 = Account("0003", "saving", "Mark Hill", 3000)
+account4 = Account("0004", "saving", "David Wood", 4000)
+account5 = Account("0004", "saving", "David Wood", 4000)
+my_account_DB = AccountDB()
+my_account_DB.insert(account1)
+my_account_DB.insert(account2)
+my_account_DB.insert(account3)
+my_account_DB.insert(account4)
+my_account_DB.insert(account5)
+print('before deleted')
+print(my_account_DB)
+my_account_DB.delete_account(account1)
+print('after deleted')
+print(my_account_DB)
